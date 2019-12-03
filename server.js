04729cac07e42675aefa74df5abad6b5aca45fc2
Subrata Sarker <subrata_sarker@yahoo.com>
2019-12-03 22:45:15 +0900

    Registration of new users

--- server.js
+++ server.js
@@ -67,13 +67,44 @@ mongo.connect(process.env.DATABASE,
 
       app.route('/')
         .get((req, res) => {
-          res.render(process.cwd() + '/views/pug/index', { title: 'Home page', message: 'Please login', showLogin: true });
+          res.render(process.cwd() + '/views/pug/index', { title: 'Home page', message: 'Please login', showLogin: true, showRegistration: true });
         });
+
+      app.route('/register')
+        .post((req, res, next) => {
+          db.collection('users').findOne({ username: req.body.username },
+            (err, user) => {
+              if (err) {
+                next(err);
+              }
+              else if (user) {
+                res.redirect('/');
+              }
+              else {
+                db.collection('users').insertOne({
+                  username: req.body.username,
+                  password: req.body.password
+                }, (err, doc) => {
+                  if (err) {
+                    res.redirect('/');
+                  }
+                  else {
+                    next(null, user);
+                  }
+                })
+              }
+            })
+        },
+          passport.authenticate('local', { failureRedirect: '/' }),
+          (req, res, next) => {
+            res.redirect('/profile');
+          }
+        );
 
       app.route('/login')
         .post(passport.authenticate('local', { failureRedirect: '/' }),
           (req, res) => {
-            res.render(process.cwd() + '/views/pug/profile', {});
+            res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
           });
 
       function ensureAuthenticated(req, res, next) {
