4a4a5b2e1b6973dd3ab463e2b8b3d796c21366fc
Subrata Sarker <subrata_sarker@yahoo.com>
2019-12-03 22:28:38 +0900

    Logging a user out

--- server.js
+++ server.js
@@ -88,6 +88,18 @@ mongo.connect(process.env.DATABASE,
           res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
         });
 
+      app.route('/logout')
+        .get((req, res) => {
+          req.logout();
+          res.redirect('/');
+        })
+
+      app.use((req, res, next) => {
+        res.status(404)
+          .type('text')
+          .send('Not Found');
+      })
+
       app.listen(process.env.PORT || 3000, () => {
         console.log("Listening on port " + process.env.PORT);
       });
