68506496d714f741943ebcd94ec18d8c440e4b3c
Subrata Sarker <subrata_sarker@yahoo.com>
2019-12-03 21:45:07 +0900

    Create a middleware ensureAuthenticated

--- server.js
+++ server.js
@@ -67,14 +67,26 @@ mongo.connect(process.env.DATABASE,
 
       app.route('/')
         .get((req, res) => {
-          res.render(process.cwd() + '/views/pug/index.pug', { title: 'Hello', message: 'Please login', showLogin: true });
+          res.render(process.cwd() + '/views/pug/index', { title: 'Home page', message: 'Please login', showLogin: true });
         });
 
       app.route('/login')
         .post(passport.authenticate('local', { failureRedirect: '/' }),
           (req, res) => {
-            res.render(process.cwd() + '/views/pug/profile.pug', {});
+            res.render(process.cwd() + '/views/pug/profile', {});
           });
+
+      function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+          return next();
+        }
+        res.redirect('/');
+      };
+
+      app.route('/profile')
+        .get(ensureAuthenticated, (req, res) => {
+          res.render(process.cwd() + '/views/pug/profile');
+        });
 
       app.listen(process.env.PORT || 3000, () => {
         console.log("Listening on port " + process.env.PORT);
