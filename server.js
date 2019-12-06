ab63ed5d9837639cc881a5a28b8973744c20d535
Subrata Sarker <subrata_sarker@yahoo.com>
2019-12-06 19:46:29 +0900

    require passport-github in the server.js

--- server.js
+++ server.js
@@ -5,7 +5,30 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 const mongo = require('mongodb').MongoClient;
+const GitHubStrategy = require('passport-github').Strategy;
 
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+  (accessToken, refreshToken, profile, done) => {
+    db.collection('users').findOne({ username: profile.username }, (err, user) => {
+      console.log('User ' + profile.username + ' attempted to log in.');
+      if (err) { return done(err); }
+      if (!user) {
+        db.collection('users').insertOne({
+          username: profile.username,
+          authenticate: 'github'
+        }, (err, doc) => {
+          if (err) { return done(err); }
+          return done(null, doc.ops[0]);
+        })
+      }
+      return done(null, user);
+    });
+  })
+);
 
 const auth = require('./auth');
 const routes = require('./routes');
